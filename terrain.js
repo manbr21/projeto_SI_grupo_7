2232ebba73cd125d436f93432908481c0c694285
Marcelo Arcoverde <manbr@cin.ufpe.br>
2026-04-27 11:30:30 -0300

    Add enum to represent the terrain types and a new class that links it with the environment

---
+++ terrain.js
@@ -0,0 +1,31 @@
+const TerrainType = {
+  Sand: 'Sand',
+  Quagmire: 'Quagmire',
+  Water: 'Water',
+  Obstacle: 'Obstacle'
+};
+
+class TerrainCells {
+    constructor(target, terrainType) {
+        let alpha = 50;
+        this.target = target
+        switch(terrainType) {
+            case 'Sand':
+                this.color = color(194, 178, 128, alpha);
+                this.cost = 10
+                break;
+            case 'Quagmire':
+                this.color = color(100, 110, 60, alpha);
+                this.cost = 50
+                break;
+            case 'Water':
+                this.color = color(0, 105, 148, alpha);
+                this.cost = 100
+                break;
+            default:
+                this.color = color(100, 100, 105, alpha);
+                this.cost = 100000000
+                break;
+        }
+    }
+}
